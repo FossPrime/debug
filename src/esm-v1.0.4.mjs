39ca4081ff8075d7282c778ccea5544fff45f64b
Ray Foss <ray@vblip.com>
2023-06-04 02:34:54 -0500

    Update and rename esm-v1.0.4.mjs to esm-v1.0.5.mjs

--- src/esm-v1.0.4.mjs
+++
@@ -1,4 +0,0 @@
-import cjsModule from './node-2.js'
-console.log('FUCK')
-export const debug = cjsModule
-export default debug
---
+++ src/esm-v1.0.5.mjs
@@ -0,0 +1,4 @@
+import { createSetup } from './node-2.js'
+console.log('FUCK')
+export const debug = createSetup
+export default debug
